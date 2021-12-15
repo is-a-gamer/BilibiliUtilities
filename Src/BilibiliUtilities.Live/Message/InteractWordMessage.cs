66c86c09dbc4457b57cbaf489011844ca3c6074f
lynn <caolin@f3game.cn>
2021-12-15 18:48:02 +0800

    玩家进入直播间的消息

---
+++ Src/BilibiliUtilities.Live/Message/InteractWordMessage.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilibiliUtilities.Live.Message
+{
+    public class InteractWordMessage : BaseMessage
+    {
+
+        /// <summary>
+        /// 用户UID
+        /// </summary>
+        public long UserId;
+
+        /// <summary>
+        /// 用户名称
+        /// </summary>
+        public string Username;
+
+        /// <summary>
+        /// 勋章名称
+        /// </summary>
+        public string Medal;
+
+        /// <summary>
+        /// 勋章等级
+        /// </summary>
+        public int MedalLevel;
+
+        /// <summary>
+        /// 勋章所有者
+        /// </summary>
+        public long MedalOwnerId;
+
+        public static InteractWordMessage JsonToInteractWordMessage(JObject json)
+        {
+            if (!"INTERACT_WORD".Equals(json["cmd"].ToString()))
+            {
+                throw new ArgumentException("'cmd' 的值不是 'INTERACT_WORD'");
+            }
+
+            var data = json["data"];
+            return new InteractWordMessage
+            {
+                UserId = long.Parse(data["uid"].ToString()),
+                Username = data["uname"]?.ToString(),
+                Medal = data["fans_medal"]["medal_name"]?.ToString(),
+                MedalLevel = int.Parse(data["fans_medal"]["medal_level"]?.ToString()),
+                MedalOwnerId = long.Parse(data["fans_medal"]["target_id"].ToString()),
+                Metadata = JsonConvert.SerializeObject(json)
+            };
+        }
+
+        public static InteractWordMessage JsonToInteractWordMessage(string jsonStr)
+        {
+            try
+            {
+                var json = JObject.Parse(jsonStr);
+                return JsonToInteractWordMessage(json);
+            }
+            catch (JsonReaderException)
+            {
+                throw new AggregateException("JSON字符串没有成功转换成Json对象");
+            }
+        }
+    }
+}
---
+++ Src/BilibiliUtilities.Utils/UserUtils/UserUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BilibiliUtilities.Utils.UserUtils
+{
+    public class UserUtil
+    {
+        private static readonly HttpClient _httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(20) };
+
+        public static async Task<JToken> GetUserInfoByUid(long uid)
+        {
+            var tmpData = JObject.Parse(await _httpClient.GetStringAsync($"https://api.bilibili.com/x/space/acc/info?mid={uid}"));
+            if (int.Parse(tmpData["code"].ToString()) != 0)
+            {
+                return "";
+            }
+
+            //用户信息
+            var data = tmpData["data"];
+            return data;
+        }
+    }
+}
